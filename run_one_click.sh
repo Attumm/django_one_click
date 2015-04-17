ff8fb3c63403bf783b2e479712e30e6653eee3ef
Attumm <computerservicegemak@gmail.com>
2015-04-17 15:58:15 +0200

    small changed

--- run_one_click.sh
+++ run_one_click.sh
@@ -1,26 +1,25 @@
-!/bin/bash
+#!/bin/bash
 
+rm -R /home/django
 
 project_name=$1
 path_to_requirements=$(locate requirements.txt)
 top_folder=$(python /home/django_one_click/print_top_folder.py $1)
 
 updatedb
-apt-get update
-apt-get upgrade
+#apt-get update
+#apt-get upgrade
 
 # install for postgres
 apt-get install python-psycopg2
-apt-get install libpq-d
+apt-get install libpq-dev
 
 path_to_requirements=$(locate requirements.txt)
 
 pip install -r $path_to_requirements
 
-#chmod 777 /home/run_script.py
-python /home/run_script.py project_name
+python /home/django_one_click/run_script.py $project_name
 
-
-chown -R django:django $top_folder
+chown -R django:django django/ $top_folder
 service nginx restart
 service gunicorn restart
